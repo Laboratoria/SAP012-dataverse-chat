65b55ae6731bca05bef48260f32a01138e6d5923
renatinhafront <re.saraiva09@hotmail.com>
2024-02-15 18:23:48 -0300

    chore(init): Adds project files from curriculum

---
+++ test/data.js
@@ -0,0 +1,80 @@
+// Aqui puedes agregar los datos que necesites para tus pruebas
+// al menos debe tener uno que se llame "data"
+
+export const data = [
+  {
+    "id": "the-shawshank-redemption",
+    "name": "Um Sonho de Liberdade",
+    "shortDescription": "Prisioneiro busca liberdade em Um Sonho de Liberdade.",
+    "description": "Um banqueiro injustamente condenado à prisão busca a redenção através de uma amizade inesperada com um colega de cela. Este épico drama, dirigido por Frank Darabont, explora a esperança, a amizade e a busca pela liberdade. Ambientado em Shawshank, uma prisão sombria, o filme mergulha nos desafios enfrentados pelo protagonista, Andy Dufresne, enquanto ele enfrenta injustiças, forja conexões inesperadas e mantém a esperança contra todas as probabilidades. Com uma impressionante pontuação de 9.3 no IMDb, Um Sonho de Liberdade permanece como um testemunho poderoso da resiliência humana e da busca pela verdadeira redenção.",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@.jpg",
+    "facts": {
+      "releaseYear": 1994,
+      "runtimeStr": "2h 22min",
+      "imDbRating": 9.3,
+      "directorsName": "Frank Darabont",
+      "movieStars": "Tim Robbins, Morgan Freeman",
+      "movieGender": "drama"
+    }
+  },
+  {
+    "id": "the-godfather",
+    "name": "O Poderoso Chefão",
+    "shortDescription": "Família mafiosa liderada por Vito Corleone em O Poderoso Chefão.",
+    "description": "O Poderoso Chefão, dirigido por Francis Ford Coppola, é uma jornada monumental através do submundo da máfia italiana. Lançado em 1972, o filme segue Michael Corleone, interpretado por Al Pacino, enquanto ele é gradualmente envolvido nos intricados negócios da família após eventos violentos. Com uma pontuação de 9.2 no IMDb, O Poderoso Chefão é uma obra-prima aclamada por suas performances excepcionais, roteiro brilhante e cinematografia marcante. A narrativa rica e complexa explora temas de lealdade, poder e tragédia, solidificando seu lugar como um dos maiores filmes da história do cinema.",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@.jpg",
+    "facts": {
+      "releaseYear": 1972,
+      "runtimeStr": "2h 55min",
+      "imDbRating": 9.2,
+      "directorsName": "Francis Ford Coppola",
+      "movieStars": "Marlon Brando, Al Pacino",
+      "movieGender": "policial, drama"
+    }
+  },
+  {
+    "id": "12-angry-men",
+    "name": "12 Homens e uma Sentença",
+    "shortDescription": "Deliberação de júri em caso de homicídio em 12 Homens e uma Sentença.",
+    "description": "12 Homens e uma Sentença, dirigido por Sidney Lumet, é um drama judicial intenso que se desenrola em uma sala de deliberação. Lançado em 1957, o filme examina o julgamento de um jovem acusado de assassinato, enquanto doze jurados confrontam suas próprias dúvidas e preconceitos. Com uma pontuação de 9.0 no IMDb, 12 Homens e uma Sentença é uma obra-prima atemporal que explora a complexidade da verdade, moralidade e responsabilidade individual, oferecendo uma reflexão profunda sobre o sistema judicial e a natureza humana.",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BODRmNWRiYmUtNGM2Yy00NTgwLTgxNzMtMGExYzI5NjllNjBhXkEyXkFqcGdeQXVyMTAzMDM4MjM@.jpg",
+    "facts": {
+      "releaseYear": 1957,
+      "runtimeStr": "1h 36min",
+      "imDbRating": 9.1,
+      "directorsName": "Sidney Lumet",
+      "movieStars": "Henry Fonda, Lee J. Cobb",
+      "movieGender": "drama"
+    }
+  },
+  {
+    "id": "inception",
+    "name": "A Origem",
+    "shortDescription": "Roubo de ideias através dos sonhos em A Origem.",
+    "description": "A Origem, dirigido por Christopher Nolan, é um thriller de ficção científica que mergulha nas complexidades da mente humana. Lançado em 2010, o filme segue Dom Cobb, interpretado por Leonardo DiCaprio, um especialista em extrair segredos do subconsciente enquanto as pessoas sonham. Com uma pontuação de 8.8 no IMDb, A Origem é conhecido por sua trama intricada, efeitos visuais deslumbrantes e performances envolventes. A narrativa desafia as fronteiras entre realidade e ilusão, explorando temas de culpa, redenção e a natureza fluida da percepção.",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@.jpg",
+    "facts": {
+      "releaseYear": 2010,
+      "runtimeStr": "2h 28min",
+      "imDbRating": 8.8,
+      "directorsName": "Christopher Nolan",
+      "movieStars": "Leonardo DiCaprio, Joseph Gordon-Levitt",
+      "movieGender": "aventura, syfy"
+    }
+  },
+  {
+    "id": "forrest-gump",
+    "name": "Forrest Gump",
+    "shortDescription": "Jornada épica de Forrest Gump através de décadas da história americana.",
+    "description": "Forrest Gump, dirigido por Robert Zemeckis, é uma jornada épica que segue a vida extraordinária de um homem comum. Lançado em 1994, o filme estrela Tom Hanks como Forrest Gump, um homem com um coração puro e uma capacidade única de estar no lugar certo na hora certa. Com uma pontuação de 8.8 no IMDb, Forrest Gump é uma mistura cativante de comédia, drama e momentos históricos. A narrativa habilmente entrelaça a história de Forrest com eventos-chave da história americana, proporcionando uma experiência emotiva e inesquecível.",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BNWFmMjA4MTAtNzRjNC00MWRlLWEzMTEtYzJhYjZkMzhlMTQyXkEyXkFqcGdeQXVyMTQxNzMzNDI@.jpg",
+    "facts": {
+      "releaseYear": 1994,
+      "runtimeStr": "2h 22min",
+      "imDbRating": 8.6,
+      "directorsName": "Robert Zemeckis",
+      "movieStars": "Tom Hanks, Robin Wright",
+      "movieGender": "drama, Romance"
+    }
+  },
+];
